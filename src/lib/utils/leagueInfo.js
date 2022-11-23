e97957784a79bb00c46e45ffe7ec032d8ec9bf29
Sasch06 <118907957+Sasch06@users.noreply.github.com>
2022-11-23 16:18:07 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,7 +28,7 @@ export const managers = [
        "tookOver": "2020", // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Frankfurt", // (optional)
        "bio": "Your 2021 Champion!",
-       "photo": "saschaNeu.jpeg", // square ratio recommended (no larger than 500x500)
+       "photo": "saschaNeu.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": "2016", // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "pit", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
