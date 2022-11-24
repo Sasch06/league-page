007286729f211805ab0544b4d30aef675cc4b703
Sasch06 <118907957+Sasch06@users.noreply.github.com>
2022-11-24 15:32:53 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,11 +25,11 @@ export const managers = [
      {
        "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
        "name": "Sascha",
-       "tookOver": "2020", // (optional) used if a manager took over a team, delete this line or change to null otherwise
+       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Frankfurt", // (optional)
        "bio": "Your 2021 Champion!",
-       "photo": "saschaNeu.jpg", // square ratio recommended (no larger than 500x500)
-       "fantasyStart": "2016", // (optional) when did the manager start playing fantasy football
+       "photo": "/managers/saschaNeu.jpg", // square ratio recommended (no larger than 500x500)
+       "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "pit", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
